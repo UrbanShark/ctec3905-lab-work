baf0357c1f6ce1036d636b2fcbcd3776ea0a724a
UrbanShark <sufyaan__162@hotmail.com>
2022-02-07 15:06:12 +0000

    Staging Lab work to save

--- lab-04/scripts.js
+++ lab-04/scripts.js
@@ -1,9 +1,3 @@
-menuToggler.addEventListener('click' ev =>) {
-  menuToggler.classlist.toggle('open');
+menuToggler.addEventListener('click', ev => {
+  menu.classList.toggle('open');
 });
-
-for (const element of doucment.querySelectorAll('nav a')) {
-  element.addEventListener('clicl', ev => {
-    menuToggler.classList.remove('open');
-  })
-}
